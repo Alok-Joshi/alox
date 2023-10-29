456b2b9d47708e67693faae25cbe2b0dae3355e2
Alok Joshi <AlokJoshi523@gmail.com>
2023-10-29 19:18:10 +0530

    Formatting

--- environment.cpp
+++ environment.cpp
@@ -9,45 +9,46 @@ using namespace tok;
 //SYMBOL TABLE IMPLEMENTATIONS
 
 
-symbol_table_entry symbol_table:: get_entry(tok:: token symbol){
-
-    
-        int stack_top = this->scopes.size()-1;
-
-        while(stack_top>=0){
-
-        
-            if(this->scopes[stack_top].count(symbol.lexeme)) return this->scopes[stack_top][symbol.lexeme];
-            else stack_top--;
-
-        }
+symbol_table_entry symbol_table:: get_entry(tok:: token symbol) {
 
 
-        throw "Entry not found";
+    int stack_top = this->scopes.size()-1;
 
-}bool symbol_table:: resolve_identifier(tok:: token identifier){
+    while(stack_top>=0) {
 
-    
-        int stack_top = this->scopes.size()-1;
 
-        while(stack_top>=0){
+        if(this->scopes[stack_top].count(symbol.lexeme)) return this->scopes[stack_top][symbol.lexeme];
+        else stack_top--;
 
-        
-            if(this->scopes[stack_top].count(identifier.lexeme)) return true;
-            else stack_top--;
+    }
 
-        }
 
-        return false;
-}
-
-bool symbol_table:: is_redeclaration(tok:: token identifier){
-
-            return this->scopes[scopes.size()-1].count(identifier.lexeme);
+    throw "Entry not found";
 
 }
+bool symbol_table:: resolve_identifier(tok:: token identifier) {
 
-void symbol_table:: start_scope(token name){
+
+    int stack_top = this->scopes.size()-1;
+
+    while(stack_top>=0) {
+
+
+        if(this->scopes[stack_top].count(identifier.lexeme)) return true;
+        else stack_top--;
+
+    }
+
+    return false;
+}
+
+bool symbol_table:: is_redeclaration(tok:: token identifier) {
+
+    return this->scopes[scopes.size()-1].count(identifier.lexeme);
+
+}
+
+void symbol_table:: start_scope(token name) {
 
 
     this->function_tracker.push_back(name);
@@ -55,64 +56,64 @@ void symbol_table:: start_scope(token name){
 
 }
 
-void symbol_table:: start_scope(){
+void symbol_table:: start_scope() {
 
     map<string,symbol_table_entry> new_scope;
     this->scopes.push_back(new_scope);
 
 }
 
-void symbol_table:: end_scope(bool is_function_scope){
+void symbol_table:: end_scope(bool is_function_scope) {
 
     this->scopes.pop_back();
 
-    if(is_function_scope){
-        
+    if(is_function_scope) {
+
         this->function_tracker.pop_back();
 
     }
 
 }
-void symbol_table:: modify_entry(token symbol, symbol_table_entry symbol_information){
+void symbol_table:: modify_entry(token symbol, symbol_table_entry symbol_information) {
     //API specifically designed for declaration statements
-            scopes[scopes.size()-1][symbol.lexeme] = symbol_information;
+    scopes[scopes.size()-1][symbol.lexeme] = symbol_information;
 
 }
 
 
 
-void symbol_table:: add_entry(token symbol, symbol_table_entry symbol_information){
+void symbol_table:: add_entry(token symbol, symbol_table_entry symbol_information) {
     //API specifically designed for declaration statements
 
-        int top_index = scopes.size()-1;
-        if(scopes[top_index].count(symbol.lexeme)){
-            //implies identifier declared before
+    int top_index = scopes.size()-1;
+    if(scopes[top_index].count(symbol.lexeme)) {
+        //implies identifier declared before
 
-            throw "redeclaration error";
-        }
-        else
-        {
-            scopes[top_index][symbol.lexeme] = symbol_information;
-        }
+        throw "redeclaration error";
+    }
+    else
+    {
+        scopes[top_index][symbol.lexeme] = symbol_information;
+    }
 }
 
 
-token symbol_table:: get_current_function(){
+token symbol_table:: get_current_function() {
 
 
-        if(this->function_tracker.size() == 0){
+    if(this->function_tracker.size() == 0) {
 
-            throw "No Current Function"; //TODO: Improve this during error handling
+        throw "No Current Function"; //TODO: Improve this during error handling
 
 
-        }
+    }
 
-        else
-        {
-            
-            int top_index = this->function_tracker.size() - 1;
-            return this->function_tracker[top_index];
-        }
+    else
+    {
+
+        int top_index = this->function_tracker.size() - 1;
+        return this->function_tracker[top_index];
+    }
 
 }
 
@@ -126,7 +127,7 @@ symbol_table_entry:: symbol_table_entry() {}
 
 
 
-            
+
 
 
 
